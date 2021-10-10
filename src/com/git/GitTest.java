cc2028e2cbe2cb8d05bc246f89bbf068292e7cba
zx <zx@aaa.com>
2021-10-10 11:49:15 +0800

    222

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -1,4 +1,10 @@
 package com.git;
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+    }
 }
